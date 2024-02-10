6b12e536ac51da6a4c5bb13ca2b2fd5f5382c444
Yana Parashar <107571592+yana24parashar@users.noreply.github.com>
2024-02-10 22:05:47 +0530

    Update user.js router

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,16 @@
 import express from 'express'; 
-import {fetchUser, fetchUserbyId , addUser,editUser,deleteUser} from '../controllers/user.js'; 
+ const authcontroller= require('../controllers/user.js'); 
 
 
 const router = express.Router(); 
 
-router.get('/', fetchUser);
+router.route("login").post(authcontroller.register);
+
+/*router.get('/', fetchUser);
 router.get('/:id', fetchUserbyId );
 router.post('/', addUser);
 router.put('/:id', editUser);
 router.delete('/:id', deleteUser);
-
+*/
 
 export default router; 
