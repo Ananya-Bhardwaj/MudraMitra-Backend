04ad964da26365efacefe05f58a3cf5381833e2e
Yana Parashar <107571592+yana24parashar@users.noreply.github.com>
2024-02-10 22:30:08 +0530

    Update registration.js

--- routes/registration.js
+++ routes/registration.js
@@ -4,7 +4,7 @@ import express from 'express';
 
 const router = express.Router(); 
 
-router.route("registration").post(authcontroller.register);
+router.route("registration").post(authcontroller.login);
 
 /*router.get('/', fetchUser);
 router.get('/:id', fetchUserbyId );
