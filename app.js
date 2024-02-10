a29ee83d7f35d7de69aa7e4d82dea3cc8474dcab
Yana Parashar <107571592+yana24parashar@users.noreply.github.com>
2024-02-10 22:53:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,13 @@ import quizRouter from "./routes/quiz.js";
 import userRouter from "./routes/user.js";
 import modulesRouter from "./routes/modules.js";
 import mentorRouter from "./routes/mentor.js";
+import registrationRouter from "./routes/registration.js";
 
 app.use("/api/quiz", quizRouter);
 app.use("/api/user", userRouter);
 app.use("/api/modules", modulesRouter);
 app.use("/api/mentor", mentorRouter);
+app.use("/api/registration,registrationRouter);
 
 connectDb().then(() => {
   app.listen(5500, () => {
