70bad92d5d28a5f012d789c7737ad4c40b8ffeaa
Yana Parashar <107571592+yana24parashar@users.noreply.github.com>
2024-02-10 22:09:25 +0530

    Update registration.js

--- routes/registration.js
+++ routes/registration.js
@@ -1,5 +1,16 @@
+import express from 'express'; 
+ const authcontroller= require('../controllers/registration.js'); 
+
+
 const router = express.Router(); 
 
-router.route("/").get((req, res)=>{
-  res.status(200).send("wwelcome to registration page");
-});
+router.route("registration").post(authcontroller.register);
+
+/*router.get('/', fetchUser);
+router.get('/:id', fetchUserbyId );
+router.post('/', addUser);
+router.put('/:id', editUser);
+router.delete('/:id', deleteUser);
+*/
+
+export default router; 
