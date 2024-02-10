e9d4f61989371c03c3985e163d5dfcde07c9df72
Yana Parashar <107571592+yana24parashar@users.noreply.github.com>
2024-02-10 22:25:21 +0530

    Update registration.js

--- controllers/registration.js
+++ controllers/registration.js
@@ -1,8 +1,22 @@
 const register=async (req , res)=>{
   try{
-    
-  }catch{
-    
+    const {email , password}=req.body;
+    const userExist= await User.findone({ email });
+    console.log(userExist);
+    if(!userExist){
+      return res.status(400).json({message :"Invalid Credentials"});
+    }
+    if(user){
+      req.status(200).json({
+        msg:"registration successful",
+        
+      });
+      else{
+      res.status(401).json({message : "Invalid email or password"})
+    }
+    }
+    }catch(error){
+    res.status(500).json("internal server error");
   }
   
 }
