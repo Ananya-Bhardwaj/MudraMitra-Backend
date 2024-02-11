9dc4a080c4a170a0bbf9aeb86cbbb6da7273bb10
Yana Parashar <107571592+yana24parashar@users.noreply.github.com>
2024-02-11 11:30:21 +0530

    Update registration.js

--- controllers/registration.js
+++ controllers/registration.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js";
-const register=async (req , res)=>{
+ const register=async (req , res)=>{
   try{
     const {email , password}=req.body;
     const userExist= await User.findone({ email });
@@ -7,18 +7,20 @@ const register=async (req , res)=>{
     if(!userExist){
       return res.status(400).json({message :"Invalid Credentials"});
     }
-    if(user){
+    if( User){
       req.status(200).json({
         msg:"registration successful",
         
       });
-      else{
-      res.status(401).json({message : "Invalid email or password"})
+   
     }
+    else{
+      res.status(401).json({message : "Invalid email or password"})
     }
     }catch(error){
     res.status(500).json("internal server error");
   }
   
 }
-module.exports={'register'}
+export default register
+
