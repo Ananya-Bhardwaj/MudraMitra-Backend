5bf0aaa67ecdfd481ada81f35e46e8bffb4fd8b3
ananya <ananya.bhar@gmail.com>
2024-02-11 18:13:55 +0530

    added payments services

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import connectDb from "./utils/db.js";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv"; 
+import stripePackage from "stripe";
+import { v4 as uuidv4 } from "uuid";
+
+dotenv.config(); 
 
 const app = express();
+
+const stripe = stripePackage(process.env.STRIPE_KEY);
 
 //routes
 app.use(express.json());
@@ -29,8 +36,51 @@ app.use("/api/mentor", mentorRouter);
 app.use("/api/registration",registrationRouter);
 app.use("/api/posts", postsRouter); 
 
+
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("Product", product);
+  console.log("Price", product.price);
+
+  const idempotencyKey = uuidv4();
+
+  stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      return stripe.charges.create(
+        {
+          amount: product.price * 100, // assuming price is in cents
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,
+            },
+          },
+        },
+        { idempotencyKey },
+      );
+    })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while processing the payment" });
+    });
+});
+
 connectDb().then(() => {
   app.listen(5500, () => {
     console.log("Connected to backend, listening on port 5500");
   });
 });
+
