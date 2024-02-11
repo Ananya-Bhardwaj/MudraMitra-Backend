e298b828322f9ff888168f22cf2e7f9c8d64452a
ananya <ananya.bhar@gmail.com>
2024-02-11 16:09:24 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 import connectDb from "./utils/db.js";
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 
 const app = express();
 
 //routes
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin:
-//       "http://localhost:5173",
-//     credential: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin:
+      "https://a1305015-b261-4376-be9b-862aa25e67ec-00-2vy6071yo9zgs.sisko.replit.dev/",
+    credential: true,
+  }),
+);
 
 import quizRouter from "./routes/quiz.js";
 import userRouter from "./routes/user.js";
